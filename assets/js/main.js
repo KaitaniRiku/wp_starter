1a2907d84967667e84523c66340f3f6a6549aea5
TIBO <thibaud.allie@hotmail.fr>
2017-06-27 14:19:41 +0200

    init slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 import config from './config'
-
+import slider from './slider'
 import event from 'dom-events'
 import classes from 'dom-classes'
 import sniffer from 'sniffer'
@@ -8,4 +8,4 @@ sniffer.addClasses(document.documentElement)
 
 config.isMobile && event.on(config.$body, 'touchmove', (e) => e.preventDefault())
 
-
+slider.init()
